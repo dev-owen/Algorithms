2a9b5c9186b32d3626fbcc68fb11fb60599ab79f
Wonjong Oh <39744246+wonjongOh94@users.noreply.github.com>
2018-08-06 12:13:43 +0900

    Baekjoon / 10815 / Solved!

--- BS.java
+++ BS.java
@@ -1,12 +1,14 @@
-package _180806;
-
+*
+*
+*
+*
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
+public class BS {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
